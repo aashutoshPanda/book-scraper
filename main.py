b6ea3494bd2e681a61b9ecab7aa5a9dc0529a30a
aashutoshPanda <iamashutoshpanda@gmail.com>
2020-09-24 22:24:31 +0530

    got all urls for 1000 books

---
+++ main.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+
+from utils import get_nth_page_url
+
+count = 0
+for page_no in range(1, 51):
+    page_url = get_nth_page_url(page_no)
+    page = requests.get(page_url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    # going through each image-container div to get link for each book
+    for image_container in soup.find_all('div', class_='image_container'):
+        anchor_tag = image_container.find('a')
+        link = anchor_tag['href']
+        print(link)
+        count += 1
+print(count)
---
+++ utils.py
@@ -0,0 +1,2 @@
+def get_nth_page_url(n):
+    return f"http://books.toscrape.com/catalogue/page-{n}.html"
