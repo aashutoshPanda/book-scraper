6b58ed33be7ba51e9f2e088dad3e3a718f3cd821
aashutoshPanda <iamashutoshpanda@gmail.com>
2020-09-25 00:41:26 +0530

    refactoring

--- Book.py
+++ Book.py
@@ -1,54 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
-from utils import get_nth_page_url, get_price, get_rating, get_stock, get_category
-
-
-# getting category of book
-# category_block = soup.find_all('ul', class_='breadcrumb')[0]
-# category = get_category(category_block.find_all('li')[2].get_text())
-
-# description_article = soup.find_all('article', class_='product_page')[0]
-# description_paragraphs = description_article.find_all('p')
-
-
-# description = description_paragraphs[3].get_text()
-# rating = get_rating(description_paragraphs[2]['class'][1])
-
-
-# title = soup.find_all('h1')[0].get_text()
-# print(title)
-
-
-# tds = soup.find_all('td')
-
-# UPC = tds[0].get_text()
-# price_without_tax = get_price(tds[2].get_text())
-# price_with_tax = get_price(tds[3].get_text())
-# tax = get_price(tds[4].get_text())
-# stock = get_stock(tds[5].get_text())
-# count_reviews = tds[6].get_text()
-
-# print(UPC, price_without_tax, price_with_tax, tax, stock, count_reviews)
-
-# book_data = {
-#     'title': title,
-#     'category': category,
-#     'UPC': UPC,
-#     'price_without_tax': price_without_tax,
-#     'price_with_tax': price_with_tax,
-#     'tax': tax,
-#     'stock': stock,
-#     'count_reviews': count_reviews,
-#     'rating': rating
-# }
-
-# print(book_data)
 
 
 class Book:
-    def __init__(self, book_index):
-        self.book_index = book_index
-        self.url = self.get_nth_page_url(book_index)
+    def __init__(self, link):
+        self.url = self.get_book_url(link)
 
     def get_nth_page_url(self, n):
         """ makes url from the index """
@@ -87,23 +43,32 @@ class Book:
         """
         return str[1:-1]
 
+    def get_book_url(self, link):
+        """ Making a url for a book like this
+            http://books.toscrape.com/catalogue/running-with-scissors_215/index.html
+        """
+        return "http://books.toscrape.com/catalogue/" + link
+
     def get_book_data(self):
         page = requests.get(self.url)
         soup = BeautifulSoup(page.content, 'html.parser')
 
-        # getting category of book
+        """ Category """
         category_block = soup.find_all('ul', class_='breadcrumb')[0]
-        # category = get_category(category_block.find_all('li')[2].get_text())
+        category = self.get_category(
+            category_block.find_all('li')[2].get_text())
 
+        """ Description & Rating """
         description_article = soup.find_all(
             'article', class_='product_page')[0]
         description_paragraphs = description_article.find_all('p')
-
         description = description_paragraphs[3].get_text()
         rating = self.get_rating(description_paragraphs[2]['class'][1])
 
+        """ Title """
         title = soup.find_all('h1')[0].get_text()
 
+        """ UPC & other details """
         tds = soup.find_all('td')
 
         UPC = tds[0].get_text()
@@ -115,7 +80,7 @@ class Book:
 
         book_data = {
             'title': title,
-            # 'category': category,
+            'category': category,
             'UPC': UPC,
             'price_without_tax': price_without_tax,
             'price_with_tax': price_with_tax,
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
+from Book import Book
 
-from utils import get_nth_page_url
 
-count = 0
-for page_no in range(1, 51):
-    page_url = get_nth_page_url(page_no)
+TOTAL_PAGES = 50
+for page_no in range(1, TOTAL_PAGES+1):
+    page_url = f"http://books.toscrape.com/catalogue/page-{page_no}.html"
     page = requests.get(page_url)
     soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -13,7 +13,6 @@ for page_no in range(1, 51):
     for image_container in soup.find_all('div', class_='image_container'):
         anchor_tag = image_container.find('a')
         link = anchor_tag['href']
-        print(link)
-        count += 1
-
-print(count)
+        book = Book(link)
+        data = book.get_book_data()
+        print(data)
