2a3c3bf2b39b763eb24501b58a1064d201d2a8e9
aashutoshPanda <iamashutoshpanda@gmail.com>
2020-09-25 01:26:07 +0530

    writing to CSV

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 from Book import Book
 
 
 TOTAL_PAGES = 50
-for page_no in range(1, TOTAL_PAGES+1):
-    page_url = f"http://books.toscrape.com/catalogue/page-{page_no}.html"
-    page = requests.get(page_url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+CSV_FILE_NAME = 'book_dataset.csv'
+FIELDNAMES = ['title', 'category', 'UPC', 'price_without_tax',
+              'price_with_tax', 'tax', 'stock', 'count_reviews', 'rating', 'description']
 
-    # going through each image-container div to get link for each book
-    for image_container in soup.find_all('div', class_='image_container'):
-        anchor_tag = image_container.find('a')
-        link = anchor_tag['href']
-        book = Book(link)
-        data = book.get_book_data()
-        print(data)
+
+with open(CSV_FILE_NAME, mode='w') as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=FIELDNAMES)
+    writer.writeheader()
+    for page_no in range(1, TOTAL_PAGES+1):
+        page_url = f"http://books.toscrape.com/catalogue/page-{page_no}.html"
+        page = requests.get(page_url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        # going through each image-container div to get link for each book
+        for image_container in soup.find_all('div', class_='image_container'):
+            anchor_tag = image_container.find('a')
+            link = anchor_tag['href']
+            book = Book(link)
+            data = book.get_book_data()
+            writer.writerow(data)
